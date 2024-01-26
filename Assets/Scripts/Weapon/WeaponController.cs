309b4df7bde9ee57f0b8f1fd39bd0290ec769665
Serdar Tufan <serdartufan4@gmail.com>
2024-01-26 18:59:19 +0300

    Initial Commit

---
+++ Assets/Scripts/Weapon/WeaponController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    [SerializeField] private GameObject playerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gameObject.transform.forward = (playerCamera.transform.forward);
+        
+    }
+}
